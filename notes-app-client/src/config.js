ea4083fde15b89c74a59052c1edbd74c67cea702
Josef Norlin <23150232+josefnorlin@users.noreply.github.com>
2020-10-06 08:52:04 +0200

    Added amplify configs

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "20200808-notes-app-upload"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://6y32fq26ic.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_kJ8v6Zpwo",
+    APP_CLIENT_ID: "5cgna39hr4h817sq21lq92vasm",
+    IDENTITY_POOL_ID: "us-east-1_kJ8v6Zpwo"
+  }
+};
