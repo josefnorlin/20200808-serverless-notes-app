83a6c66f95874fae925bccaaa1410e5e0393c3e8
Josef Norlin <23150232+josefnorlin@users.noreply.github.com>
2020-10-19 09:58:03 +0200

    Add notes and upload files to S3

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -12,6 +12,6 @@ export default {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_kJ8v6Zpwo",
     APP_CLIENT_ID: "5cgna39hr4h817sq21lq92vasm",
-    IDENTITY_POOL_ID: "us-east-1_kJ8v6Zpwo"
-  }
+    IDENTITY_POOL_ID: "us-east-1:bad7f3c1-4402-48b2-b5b2-d6b3bf34ac5a",
+  },
 };
--- notes-app-client/src/containers/NewNote.js
+++ notes-app-client/src/containers/NewNote.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { API } from "aws-amplify";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { onError } from "../libs/errorLib";
+import { s3Upload } from "../libs/awsLib";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./NewNote.css";
@@ -26,8 +27,9 @@ export default function NewNote() {
 
     if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
       alert(
-        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-          1000000} MB.`
+        `Please pick a file smaller than ${
+          config.MAX_ATTACHMENT_SIZE / 1000000
+        } MB.`
       );
       return;
     }
@@ -35,13 +37,16 @@ export default function NewNote() {
     setIsLoading(true);
 
     try {
-      await createNote({ content });
+      const attachment = file.current ? await s3Upload(file.current) : null;
+
+      await createNote({ content, attachment });
       history.push("/");
     } catch (e) {
       onError(e);
       setIsLoading(false);
     }
   }
+
 
   function createNote(note) {
     return API.post("notes", "/notes", {
